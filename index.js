f39f4adca8a18ce7c9845a87814a0f62e0576e7a
Ricardo Bravo <ricardobravo@outlook.com>
2017-02-02 10:21:19 -0500

    feat(bot): conversation added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var controller = Botkit.slackbot({
   //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
+//xoxb-135761155234-yKQdQPgRU3zW6kQJporR4dZf
 // connect the bot to a stream of messages
 controller.spawn({
   token: "xoxb-135761155234-yKQdQPgRU3zW6kQJporR4dZf",
@@ -17,3 +18,82 @@ controller.hears(['hello', 'hi', 'hola'],['direct_message','direct_mention','men
   bot.reply(message,'Hola');
 
 });
+
+controller.hears('tengo (.*)',['direct_message','direct_mention','mention'],function(bot,message) {
+  var state = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
+  if (state === 'hambre') {
+    return bot.reply(message, 'Come');
+  } else if (state === 'sueño'){
+  	return bot.reply(message, 'Duerme')
+  }
+  return bot.reply(message, 'No te entiendo');
+});
+
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Your name is ' + user.name);
+        } else {
+            bot.startConversation(message, function(err, convo) {
+                if (!err) {
+                    convo.say('I do not know your name yet!');
+                    convo.ask('What should I call you?', function(response, convo) {
+                        convo.ask('You want me to call you `' + response.text + '`?', [
+                            {
+                                pattern: 'yes',
+                                callback: function(response, convo) {
+                                    // since no further messages are queued after this,
+                                    // the conversation will end naturally with status == 'completed'
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: 'no',
+                                callback: function(response, convo) {
+                                    // stop the conversation. this will cause it to end with status == 'stopped'
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: function(response, convo) {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        convo.next();
+
+                    }, {'key': 'nickname'}); // store the results in a field called nickname
+
+                    convo.on('end', function(convo) {
+                        if (convo.status == 'completed') {
+                            bot.reply(message, 'OK! I will update my dossier...');
+
+                            controller.storage.users.get(message.user, function(err, user) {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    };
+                                }
+                                user.name = convo.extractResponse('nickname');
+                                controller.storage.users.save(user, function(err, id) {
+                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                });
+                            });
+
+
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, 'OK, nevermind!');
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
